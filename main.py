8e2411a7674b92ac8c64ca3111675e9697226284
wolfm2@newschool.edu <wolfm2@newschool.edu>
2018-03-06 15:53:09 -0500

    asdf

--- main.py
+++ main.py
@@ -1,11 +1,56 @@
 import papermill as pm
 import os
 
-os.chdir("minibook/test")
+nbk_path = "minibook/test/intro_python.ipynb" # RELATIVE to repo base!
+ret_files = []
+prep_files = True  # prep files before sending
 
-pm.execute_notebook(
-   'intro_python.ipynb',
-   'output.ipynb',
-   #parameters = dict(alpha=0.6, ratio=0.1)
-)
+def main():
+  pm.execute_notebook(
+    os.path.basename(nbk_path),
+    'output.ipynb',
+    #parameters = dict(alpha=0.6, ratio=0.1)
+  )
 
+sz_90m = 90 * (1024 ** 2)
+
+# unzip large files
+def unpack(): # zip -FF dataIn.zip --out y.zip
+  relpath = os.path.dirname(nbk_path)
+  os.system('/usr/bin/zip -FF ~/jobin/' + relpath + '/dataIn.zip --out out.zip')
+  os.system('/usr/bin/unzip -u ./out.zip -d .')
+  os.system('rm ./out.zip')
+
+# zip large files
+def pack(fn, zName): 
+  if not len(fn):
+    return
+  os.system('rm ' + zName + '.z*') # can't append split archives, so remove
+  zcall = '/usr/bin/zip ' + zName + '.zip -s 50m'
+  for f in fn: 
+    zcall += " '" + f + "'"
+  os.system(zcall)
+
+# preps large files before sending.
+def preSend():
+  print("Prepping files...\n")
+  paths  = []
+  for top, dirs, files in os.walk('.'):
+    for nm in files:
+      fn = os.path.join(top, nm)
+      if os.path.getsize(fn) > sz_90m:
+        print ("Adding: " + fn + "\n")
+        paths.append(fn)
+        
+  pack(paths, 'dataIn')
+  
+if __name__ == '__main__':
+
+  os.chdir(os.path.dirname(nbk_path))
+  if prep_files:
+    preSend()
+    exit()
+    
+  unpack()
+  main()
+  pack(ret_files, 'dataOut')
