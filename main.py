acb06f1e5dffc5d8be6294bd32ba9d674b78e44b
wolfm2@newschool.edu <wolfm2@newschool.edu>
2018-03-08 09:22:22 -0500

    system

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import papermill as pm
 import os
 
-#nbk_path = "minibook/amazon/_01_train.ipynb" # RELATIVE to repo base!
-nbk_path = "minibook/system/runner.ipynb" # RELATIVE to repo base!
+nbk_path = "minibook/amazon/_01_train.ipynb" # RELATIVE to repo base!
+#nbk_path = "minibook/system/runner.ipynb" # RELATIVE to repo base!
 #ret_files = ['../amazonFirstIter/sc.pkl', '../amazonFirstIter/hv.pkl', '../amazonFirstIter/lgs.pkl']
 ret_files = []
 prep_files = False  # prep files before sending
