10e4449008db4e1133a25f5098119d648b101426
eydu <wolfm2@newschool.edu>
2018-03-09 14:22:25 -0500

    test

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import os
 # os.system('killall -s SIGKILL -u wolfm2')
 
 #nbk_path = "minibook/amazon/_01_train.ipynb" # RELATIVE to repo base!
-nbk_path = "minibook/system/runner.ipynb" # RELATIVE to repo base!
+nbk_path = "minibook/amazon/_01_train.0.ipynb" # RELATIVE to repo base!
+#nbk_path = "minibook/system/runner.ipynb" # RELATIVE to repo base!
 #ret_files = ['../amazonFirstIter/sc.pkl', '../amazonFirstIter/hv.pkl', '../amazonFirstIter/lgs.pkl']
 ret_files = []
 prep_files = False  # prep files before sending
@@ -62,7 +63,7 @@ def preSend():
 if __name__ == '__main__':
   
   os.system('/usr/bin/rsync --delete -av ~/jobin/{0}/ ~/jobout/{0} --exclude dataIn.??'.format(os.path.dirname(nbk_path)))
-  print('/usr/bin/rsync --delete -av {0}/ ~/jobout/{0} --exclude dataIn.??'.format(os.path.dirname(nbk_path)))
+  #print('/usr/bin/rsync --delete -av {0}/ ~/jobout/{0} --exclude dataIn.??'.format(os.path.dirname(nbk_path)))
   os.chdir(os.path.dirname(nbk_path))
   os.system('mv ../job.log .')
   #os.system('ps aux > out.txt') 
