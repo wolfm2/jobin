f8b984f4cb46c7368314cffb33dc826e92ab07ae
wolfm2@newschool.edu <wolfm2@newschool.edu>
2018-03-12 16:04:33 -0400

    test

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import os
 # os.system('killall -s SIGKILL -u wolfm2')
 
 #nbk_path = "minibook/amazon/_01_train.ipynb" # RELATIVE to repo base!
+nbk_path = "minibook/amazonIter/_03_submission.ipynb" # RELATIVE to repo base!
 #nbk_path = "minibook/amazon/_01_train.0.ipynb" # RELATIVE to repo base!
 #nbk_path = "minibook/system/runner.ipynb" # RELATIVE to repo base!
-nbk_path = "minibook/amazonIter/_03_submission.ipynb" # RELATIVE to repo base!
 #ret_files = ['../amazonFirstIter/sc.pkl', '../amazonFirstIter/hv.pkl', '../amazonFirstIter/lgs.pkl']
 ret_files = []
 prep_files = False  # prep files before sending
